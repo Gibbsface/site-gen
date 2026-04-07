c5a136e7a7fd13ab9c69d6bb180bdfa0ef6da07d
Gibbsface <cole.gibbs@gmail.com>
2026-04-07 15:05:34 -0500

    init TextNode enum and class

---
+++ src/main.py
@@ -0,0 +1,7 @@
+from textnode import TextType, TextNode
+
+def main():
+    tn = TextNode("dummy text", TextType.BOLD)
+    print(tn)
+
+main()
---
+++ src/textnode.py
@@ -0,0 +1,24 @@
+from enum import Enum
+
+class TextType(Enum):
+    PLAIN = ""
+    BOLD = "**"
+    ITALIC = "_"
+    CODE = "`"
+    LINK = "[ANCHOR](URL)"
+    IMAGE = "![ALT](URL)"
+
+class TextNode:
+    def __init__(self, text, type, url=None):
+        self.text = text
+        self.type = type
+        self.url = url
+
+    def __eq__(self, other):
+        return (self.text == other.text and
+                self.type == other.type and
+                self.url == other.url)
+    
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.type}, {self.url})"
+    
