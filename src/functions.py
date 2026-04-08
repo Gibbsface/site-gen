3dddc808dd28f96210402dd9c0cd13ea8fc5035c
Gibbsface <cole.gibbs@gmail.com>
2026-04-08 15:59:37 -0500

    added function to split textnodes according to a delimeter

--- src/functions.py
+++ src/functions.py
@@ -2,7 +2,6 @@ from htmlnode import LeafNode
 from textnode import TextType, TextNode
 
 def text_node_to_html_node(text_node):
-    
     match text_node.type:
         case TextType.TEXT:
             return LeafNode(None, text_node.text, None)
@@ -18,3 +17,32 @@ def text_node_to_html_node(text_node):
             return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
         case _:
             raise Exception("invalid type")
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    ans = []
+
+    for n in old_nodes:
+        if n.type is not TextType.TEXT:
+            ans.append(n)
+            continue
+
+        tokens = n.text.split(delimiter)
+        print(tokens)
+
+        if len(tokens) % 2 == 0:
+            raise Exception("Error: unmatched delimiter")
+        
+        in_text = True
+        for t in tokens:
+            if in_text:
+                ans.append(TextNode(t, TextType.TEXT))
+                in_text = False
+            else:
+                ans.append(TextNode(t, text_type))
+                in_text = True
+                
+    return ans
+
+
+a = [TextNode("This is text with a `code block` word", TextType.TEXT)]      
+print(split_nodes_delimiter(a, "`", TextType.CODE))
